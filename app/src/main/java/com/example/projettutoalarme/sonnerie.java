5bb575991fbe6a13086a4d1ea1616e6bca419461
Cynthia <cali.cynthia30@gmail.com>
2022-03-17 17:08:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/projettutoalarme/sonnerie.java
@@ -0,0 +1,14 @@
+package com.example.projettutoalarme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sonnerie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sonnerie);
+    }
+}
